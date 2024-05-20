48f3e1e3c013bef0f91c22ce427906be0752f31f
malikjayendria <malikjayendria@gmail.com>
2024-05-20 20:04:04 +0700

    add EmailGenerator App

---
+++ src/emailapp/EmailApp.java
@@ -0,0 +1,9 @@
+package emailapp;
+
+public class EmailApp {
+    public static void main(String[] args) {
+        Email em1 = new Email("Malik", "Jayendria");
+
+        System.out.println(em1.showInfo());
+    }
+}
