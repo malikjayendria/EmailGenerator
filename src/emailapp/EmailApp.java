b9037c4653646e637229310fb0a5d128d0c848c7
malikjayendria <malikjayendria@gmail.com>
2024-05-21 15:21:51 +0700

    added Scanner in EmailApp.java

--- src/emailapp/EmailApp.java
+++ src/emailapp/EmailApp.java
@@ -1,8 +1,17 @@
 package emailapp;
 
+import java.util.Scanner;
+
 public class EmailApp {
     public static void main(String[] args) {
-        Email em1 = new Email("Malik", "Jayendria");
+        Scanner scanner = new Scanner(System.in);
+        String firstname, lastname;
+        System.out.print("input First Name: ");
+        firstname = scanner.next();
+        System.out.print("input Last Name: ");
+        lastname = scanner.next();
+
+        Email em1 = new Email(firstname, lastname);
 
         System.out.println(em1.showInfo());
     }
